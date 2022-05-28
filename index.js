88573a5d00eae5cc0f008c9beee6d5bb8051c43f
studentsaathi <ajeetsinghbhabua@gmail.com>
2022-05-29 02:21:21 +0530

    hotstar

---
+++ index.js
@@ -0,0 +1,62 @@
+let id;
+let movies_div=document.getElementById("lastest")
+
+
+ async function searchMovies(q){
+   try {
+    let url=`https://www.omdbapi.com/?s=${q}&apikey=c980f154`;
+    let res= await fetch (url)
+    let data= await res.json();
+    return data.Search;
+    //console.log(data);
+
+   }
+   catch(error){
+       console.log('error:',error)
+   }
+
+} 
+
+
+function appendMovies (movies){
+    movies_div.innerHTML=null;
+   
+    if(movies===undefined){
+        return false;
+    }
+    movies.forEach(function(el){
+        let p=document.createElement("p")
+        p.innerText=el.Title;
+        //div.append(p)
+        let img=document.createElement("img")
+        img.src=el.Poster;
+        
+        let div=document.createElement("div")
+       // div.id="mov"
+        div.append(img,p)
+        movies_div.append(div)
+    });
+
+    
+}
+
+
+
+ async function main(){
+    let query =document.getElementById("query").value;
+     let response=searchMovies(query);
+    let data =await response;
+    appendMovies(data);
+    console.log("data",data)
+
+}
+
+function debouncFunction(func,delay){
+    if(id){
+        clearTimeout(id)
+    }
+     id=setTimeout(function(){
+        func();
+    },delay);
+
+}
